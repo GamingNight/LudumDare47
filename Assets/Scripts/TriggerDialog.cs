222a1558f7a7c5ba197796b25276b18ec96c2364
JulesPomme <julian.moreira.fr@gmail.com>
2020-10-03 22:58:22 +0200

    dialog

--- Assets/Scripts/TriggerDialog.cs
+++ Assets/Scripts/TriggerDialog.cs
@@ -8,25 +8,40 @@ public class TriggerDialog : MonoBehaviour
     private TriggerHighlight triggerHighlight;
     private DialogParser parser;
     private bool dialogIsInProgress;
-
-    private int dialogCount;
-    private int lineCount;
+    private Text textUI;
+    private int dialogId;
+    private int lineId;
+    private List<string> currentDialog;
 
     private void Start() {
         triggerHighlight = GetComponent<TriggerHighlight>();
         parser = GetComponent<DialogParser>();
-        dialogCount = 0;
-        lineCount = 0;
+        dialogId = 0;
+        lineId = 0;
+        currentDialog = null;
+        textUI = null;
     }
     void Update() {
-        Text textUI;
+
         if (Input.GetKeyDown(KeyCode.Space) && triggerHighlight.IsHighLighted() && !dialogIsInProgress) {
             textUI = GameController.GetInstance().LaunchDialogBox(this);
             dialogIsInProgress = true;
+            currentDialog = parser.GetDialog(dialogId);
         }
 
         if (dialogIsInProgress) {
-
+            if (lineId == 0 || Input.GetKeyDown(KeyCode.Space)) {
+                if (lineId == currentDialog.Count) {
+                    dialogId = Mathf.Min(parser.GetDialogCount() - 1, dialogId + 1); ;
+                    GameController.GetInstance().QuitDialogBox();
+                    dialogIsInProgress = false;
+                    currentDialog = null;
+                    lineId = 0;
+                } else {
+                    textUI.text = currentDialog[lineId];
+                    lineId++;
+                }
+            }
         }
     }
 }
