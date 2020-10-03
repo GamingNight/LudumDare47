9dbda5e72164287ab847db5186f30888b7ce65e6
JulesPomme <julian.moreira.fr@gmail.com>
2020-10-03 14:28:12 +0200

    camera

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Vector2 boundaries;
+    public GameObject player;
+    
+    void Update()
+    {
+        if(player.transform.position.x > boundaries.x && player.transform.position.x < boundaries.y) {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }    
+    }
+}
