b85c987c84975dccf61f3d3443ac1889beaefd34
JulesPomme <julian.moreira.fr@gmail.com>
2020-10-03 18:43:11 +0200

    pnj dialog

---
+++ Assets/Scripts/TriggerHighlight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerHighlight : MonoBehaviour
+{
+
+    public GameObject highlightObject;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        
+        if(collision.gameObject.tag == "Player") {
+            highlightObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+
+        if (collision.gameObject.tag == "Player") {
+            highlightObject.SetActive(false);
+        }
+    }
+}
