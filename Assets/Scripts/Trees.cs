231c6badd3540492da2d7af9d8f7e909629125eb
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2020-10-03 19:49:54 +0200

    des trucs

---
+++ Assets/Scripts/Trees.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Trees : MonoBehaviour
+{
+
+    private Animator anim;
+    private float randomnumber;
+
+    void Start()
+    {
+
+        randomnumber = Random.Range(0.8f, 1.2f);
+
+        anim = GetComponent<Animator>();
+        anim.SetFloat("randomstart", randomnumber);
+        anim.speed = randomnumber;
+
+    }
+
+}
