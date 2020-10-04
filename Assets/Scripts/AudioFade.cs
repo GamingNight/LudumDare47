582cd1cbf8a04737e44152648e54b5ec41e2195f
JulesPomme <julian.moreira.fr@gmail.com>
2020-10-04 15:49:20 +0200

    flute

---
+++ Assets/Scripts/AudioFade.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioFade : MonoBehaviour
+{
+
+    public float fadeInDuration = 0.5f;
+    public float fadeOutDuration = 0.5f;
+    private AudioSource audioSource;
+    public AudioSource source { get { return audioSource; } }
+    public AudioClip clip { get { return audioSource.clip; } set { audioSource.clip = value; } }
+    public bool isPlaying { get { return audioSource.isPlaying; } }
+    public float pitch { get { return audioSource.pitch; } set { audioSource.pitch = value; } }
+
+    private float initVolume;
+    private Coroutine fadeInCoroutine;
+    private Coroutine fadeOutCoroutine;
+    private bool isFadeInRunning;
+    private bool isFadeOutRunning;
+    public bool isFadingOut { get { return isFadeOutRunning; } }
+    public bool isFadingIn { get { return isFadeInRunning; } }
+
+    private void Awake() {
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start() {
+        initVolume = audioSource.volume;
+        fadeInCoroutine = null;
+        fadeOutCoroutine = null;
+        isFadeInRunning = false;
+        isFadeOutRunning = false;
+    }
+
+    public void PlayWithFadeIn() {
+
+        bool fromFadeOut = false;
+        if (isFadeOutRunning) {
+            //Debug.Log("Interrupt Fade out");
+            StopCoroutine(fadeOutCoroutine);
+            isFadeOutRunning = false;
+            fromFadeOut = true;
+        }
+        fadeInCoroutine = StartCoroutine(FadeInCoroutine(fromFadeOut));
+    }
+
+    public void StopWithFadeOut() {
+
+        bool fromFadeIn = false;
+        if (isFadeInRunning) {
+            //Debug.Log("Interrupt Fade in");
+            StopCoroutine(fadeInCoroutine);
+            isFadeInRunning = false;
+            fromFadeIn = true;
+        }
+        fadeOutCoroutine = StartCoroutine(FadeOutCoroutine(fromFadeIn));
+    }
+
+    private IEnumerator FadeInCoroutine(bool fromFadeOut) {
+
+        //Debug.Log("Start Fade in");
+        isFadeInRunning = true;
+        float step = 0;
+        audioSource.Play();
+        while (step < fadeInDuration) {
+            audioSource.volume = Mathf.Lerp(fromFadeOut ? audioSource.volume : 0, initVolume, step / fadeInDuration);
+            step += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+        }
+        isFadeInRunning = false;
+        //Debug.Log("End Fade in");
+    }
+
+    private IEnumerator FadeOutCoroutine(bool fromFadeIn) {
+
+        //Debug.Log("Start Fade out");
+        isFadeOutRunning = true;
+        float step = 0;
+        while (step < fadeInDuration) {
+            audioSource.volume = Mathf.Lerp(fromFadeIn ? audioSource.volume : initVolume, 0, step / fadeInDuration);
+            step += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+        }
+        audioSource.Stop();
+        audioSource.volume = initVolume;
+        isFadeOutRunning = false;
+        //Debug.Log("End Fade out");
+    }
+
+    public void Play() {
+        audioSource.Play();
+    }
+
+    public void Stop() {
+        audioSource.Stop();
+    }
+}
--- Assets/Scripts/Flute.cs
+++ Assets/Scripts/Flute.cs
@@ -1,27 +1,19 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Diagnostics;
 
-[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(AudioFade))]
 public class Flute : MonoBehaviour
 {
+    private AudioFade audioFade;
+    private float note;
 
-    public AudioClip C4;
-
-    public float note;
-
-
-    void Start()
-    {
-
-        AudioSource audio = GetComponent<AudioSource>();
+    void Start() {
+        audioFade = GetComponent<AudioFade>();
+        audioFade.source.priority = 0;
         note = -1f;
-
     }
 
 
-    void Update()
-    {
+    void Update() {
 
         if (Input.GetKeyDown("x")) note = 0;
         if (Input.GetKeyDown("c")) note = 2;
@@ -29,13 +21,12 @@ public class Flute : MonoBehaviour
         if (Input.GetKeyDown("b")) note = 5;
         if (Input.GetKeyDown("n")) note = 7;
 
-        if (Input.GetKeyDown("x") || Input.GetKeyDown("c") || Input.GetKeyDown("v") || Input.GetKeyDown("b") || Input.GetKeyDown("n"))
-        {
-            GetComponent<AudioSource>().pitch = Mathf.Pow(2f, (note-4f)/ 12.0f);
-            GetComponent<AudioSource>().Play();
+        if (Input.GetKeyDown("x") || Input.GetKeyDown("c") || Input.GetKeyDown("v") || Input.GetKeyDown("b") || Input.GetKeyDown("n")) {
+            audioFade.pitch = Mathf.Pow(2f, (note - 4f) / 12.0f);
+            //audioFade.PlayWithFadeIn();
+            audioFade.Play();
+        } else if (Input.GetKeyUp("x") || Input.GetKeyUp("c") || Input.GetKeyUp("v") || Input.GetKeyUp("b") || Input.GetKeyUp("n")) {
+            audioFade.StopWithFadeOut();
         }
-
     }
-
-
 }
