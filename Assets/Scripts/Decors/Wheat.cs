a7abd4d68a1447256686b097cb4c3660dd47e690
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2020-10-04 11:24:01 +0200

    repush

---
+++ Assets/Scripts/Decors/Wheat.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Wheat : MonoBehaviour
+{
+
+
+    private SpriteRenderer spriteR;
+    public Sprite[] wheatSprites;
+    private float flipRandom;
+
+
+    void Start()
+    {
+
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+        spriteR.sprite = wheatSprites[Random.Range(0, wheatSprites.Length)];
+
+        flipRandom = Random.Range(0, 2);
+        if(flipRandom >= 1)
+        {
+            spriteR.flipX = false;
+        }
+        else
+        {
+            spriteR.flipX = true;
+        }
+
+    }
+
+
+    void Update()
+    {
+        
+
+
+    }
+}
