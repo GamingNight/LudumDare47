996dd1747d294a378d86bdf2706925497f8c53f9
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2020-10-04 20:39:04 +0200

    toukasé

--- Assets/Scripts/Decors/Trees.cs
+++ Assets/Scripts/Decors/Trees.cs
@@ -1,19 +1,48 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Trees : MonoBehaviour
 {
 
     private Animator anim;
-    private float randomnumber;
+    private float randomStartAnim;
+
+    private SpriteRenderer spriteR;
+    public bool autumnTree = false;
+
+    private Color selectedColor;
+    public Color[] summerColorsList;
+    public Color[] autumnColorsList;
+
 
     void Start()
     {
 
-        randomnumber = Random.Range(0.8f, 1.2f);
+
+        randomStartAnim = UnityEngine.Random.Range(0.8f, 1.2f);
 
         anim = GetComponent<Animator>();
-        anim.SetFloat("randomstart", randomnumber);
-        anim.speed = randomnumber;
+        anim.SetFloat("randomstart", randomStartAnim);
+        anim.speed = randomStartAnim;
+
+
+        if (autumnTree == true)
+        {
+
+            selectedColor = autumnColorsList[UnityEngine.Random.Range(0, autumnColorsList.Length)];
+
+        } else
+        {
+
+            selectedColor = summerColorsList[UnityEngine.Random.Range(0, summerColorsList.Length)];
+
+        }
+
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+        spriteR.color = selectedColor;
+
+
 
     }
 
