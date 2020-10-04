4f230133030f25798074824437953f1e8707cbf2
JulesPomme <julian.moreira.fr@gmail.com>
2020-10-04 14:03:13 +0200

    footsteps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,26 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
 
+    private AudioSource footstepSource;
     private Vector3 move;
     private bool lockController = false;
+
+    private void Start() {
+        footstepSource = GetComponent<AudioSource>();
+    }
 
     void Update() {
         if (!lockController) {
             move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
             transform.position += move * speed * Time.deltaTime;
+
+            if (move != Vector3.zero) {
+                if (!footstepSource.isPlaying)
+                    footstepSource.Play();
+            } else {
+                if (footstepSource.isPlaying)
+                footstepSource.Stop();
+            }
         }
     }
 
