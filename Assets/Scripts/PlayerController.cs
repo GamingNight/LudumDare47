8ea060d50ee32273b983a11588a8b375495f65f5
JulesPomme <julian.moreira.fr@gmail.com>
2020-10-04 15:01:56 +0200

    Speed + sprite flip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,24 +9,34 @@ public class PlayerController : MonoBehaviour
     public float speed;
 
     private AudioSource footstepSource;
+    private SpriteRenderer spriteRenderer;
     private Vector3 move;
     private bool lockController = false;
 
     private void Start() {
         footstepSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update() {
         if (!lockController) {
-            move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+
+            float horizontal = Input.GetAxis("Horizontal");
+            move = new Vector3(horizontal, 0, 0);
             transform.position += move * speed * Time.deltaTime;
+
+            if (horizontal < 0) {
+                spriteRenderer.flipX = true;
+            } else if (horizontal > 0) {
+                spriteRenderer.flipX = false;
+            }
 
             if (move != Vector3.zero) {
                 if (!footstepSource.isPlaying)
                     footstepSource.Play();
             } else {
                 if (footstepSource.isPlaying)
-                footstepSource.Stop();
+                    footstepSource.Stop();
             }
         }
     }
