ba03ed16b342e5c9348aebe707c2061ff914d939
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2020-10-05 15:08:41 +0200

    des anims

---
+++ Assets/Scripts/Decors/Autel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Autel : MonoBehaviour
+{
+
+    private bool isHighlighted;
+    private Animator anim;
+
+    private void Start()
+    {
+        isHighlighted = false;
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            isHighlighted = true;
+            anim.SetBool("playerClose", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            isHighlighted = false;
+            anim.SetBool("playerClose", false);
+        }
+    }
+
+    public bool IsHighLighted()
+    {
+        return isHighlighted;
+    }
+}
