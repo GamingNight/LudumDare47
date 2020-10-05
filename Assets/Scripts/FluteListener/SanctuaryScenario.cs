b6bfe0144863cba7a9fafa21dbb3aeaeca02c749
JulesPomme <julian.moreira.fr@gmail.com>
2020-10-05 21:25:23 +0200

    end scene + pnj music

--- Assets/Scripts/FluteListener/SanctuaryScenario.cs
+++ Assets/Scripts/FluteListener/SanctuaryScenario.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class SanctuaryScenario : AbstractFluteListenerScenario
 {
@@ -12,20 +13,48 @@ public class SanctuaryScenario : AbstractFluteListenerScenario
     public Animator[] toTrigger;
 
     private State currentState;
+    private Color playerInitColor;
+    private float waitingAnimationDuration;
+    private float colorChangeDuration;
+    private float timeSinceLastState;
 
     void Start() {
         currentState = State.NONE;
+        waitingAnimationDuration = 1.5f;
+        colorChangeDuration = 1;
+        timeSinceLastState = 0;
+        playerInitColor = GameController.GetInstance().player.GetComponent<SpriteRenderer>().color;
     }
 
     public override void InitScenario() {
         foreach (Animator animator in toTrigger) {
             animator.SetBool("succeed", true);
         }
+        GameController.GetInstance().player.GetComponent<PlayerMode>().GoToMode(PlayerMode.Mode.CONTROLLER);
+        GameController.GetInstance().player.GetComponent<PlayerController>().LockController();
+        GetComponent<AudioSource>().Stop();
         currentState = State.WAITING_FOR_ANIMATION;
+        timeSinceLastState = 0;
     }
 
     protected override void RunScenario() {
-        //Changer la couleur du player en blanc
-    }
 
+        if (currentState == State.WAITING_FOR_ANIMATION) {
+            timeSinceLastState += Time.deltaTime;
+            if (timeSinceLastState >= waitingAnimationDuration) {
+                currentState = State.PLAYER_COLOR_CHANGE;
+                timeSinceLastState = 0;
+            }
+        } else if (currentState == State.PLAYER_COLOR_CHANGE) {
+            SpriteRenderer playerSpriteRenderer = GameController.GetInstance().player.GetComponent<SpriteRenderer>();
+            Color.Lerp(playerInitColor, Color.white, timeSinceLastState / colorChangeDuration);
+            timeSinceLastState += Time.deltaTime;
+            if (timeSinceLastState >= colorChangeDuration) {
+                currentState = State.END;
+                timeSinceLastState = 0;
+            }
+        } else if (currentState == State.END) {
+            GameController.GetInstance().player.GetComponent<PlayerController>().UnlockController();
+        }
+    }
 }
