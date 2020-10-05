fc4cf93e736102b4744486b482a94a01e080f174
Leonaaard <34171080+Leonaaard@users.noreply.github.com>
2020-10-05 12:17:40 +0200

    new highlight

---
+++ Assets/Scripts/Decors/RandomColorSprite.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColorSprite : MonoBehaviour
+{
+
+
+    private SpriteRenderer spriteR;
+
+    public bool autumnSeason = false;
+
+    private Color selectedColor;
+    public Color[] summerColorsList;
+    public Color[] autumnColorsList;
+
+
+    void Start()
+    {
+
+        if (autumnSeason == true)
+        {
+
+            selectedColor = autumnColorsList[UnityEngine.Random.Range(0, autumnColorsList.Length)];
+
+        }
+        else
+        {
+
+            selectedColor = summerColorsList[UnityEngine.Random.Range(0, summerColorsList.Length)];
+
+        }
+
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+        spriteR.color = selectedColor;
+
+
+    }
+
+}
