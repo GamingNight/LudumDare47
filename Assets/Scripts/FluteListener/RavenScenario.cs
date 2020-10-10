4a20c46714e68fc2cc0c46d10d5fa8dd22a9cd3c
JulesPomme <julian.moreira.fr@gmail.com>
2020-10-10 10:51:23 +0200

    raven

---
+++ Assets/Scripts/FluteListener/RavenScenario.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RavenScenario : AbstractFluteListenerScenario
+{
+    private enum State
+    {
+        NONE, WAIT_BEFORE_FLY, FLY
+    }
+
+    private State currentState;
+    private float timeSinceLastWait;
+    private float waitDureBeforeFly;
+
+    void Start() {
+        currentState = State.NONE;
+        timeSinceLastWait = 0;
+        waitDureBeforeFly = 1;
+    }
+
+    public override void InitScenario() {
+        currentState = State.WAIT_BEFORE_FLY;
+    }
+
+    protected override void RunScenario() {
+        if (currentState == State.WAIT_BEFORE_FLY) {
+            timeSinceLastWait += Time.deltaTime;
+            if (timeSinceLastWait >= waitDureBeforeFly) {
+                timeSinceLastWait = 0;
+                currentState = State.FLY;
+            }
+        } else if (currentState == State.FLY) {
+            GetComponent<AudioSource>().Stop();
+            GetComponent<Animator>().SetBool("succeed", true);
+            timeSinceLastWait = 0;
+            currentState = State.NONE;
+        }
+    }
+}
