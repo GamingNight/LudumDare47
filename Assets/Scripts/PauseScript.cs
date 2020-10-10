bc29c2f1eb5a3ad1553a65a42fbb02540303dc7e
JulesPomme <julian.moreira.fr@gmail.com>
2020-10-10 22:32:56 +0200

    last debug

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+
+    public GameObject background;
+    public GameObject pausePanel;
+
+
+    private bool pauseIsActive;
+
+    void Start() {
+        pauseIsActive = false;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            background.SetActive(!background.activeSelf);
+            pausePanel.SetActive(!pausePanel.activeSelf);
+            pauseIsActive = !pauseIsActive;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && pauseIsActive) {
+            Application.Quit();
+        }
+    }
+}
